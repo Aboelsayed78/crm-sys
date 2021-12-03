6a74a052b9f0cb1a329c746675dc8e91e0e99436
Aboelsayed78 <aboelsaied78@yahoo.com>
2021-12-03 23:48:44 +0200

    deploy (commit: fc6b48b9c527bd5345a59f42c23aa9d1bb3752c3)

---
+++ precache-manifest.c3a1cdebcde48f404b744cd8fa3aa28a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1bffd28446508e2c14",
+    "url": "/crm-sys/css/app.311b0fea.css"
+  },
+  {
+    "revision": "47bf7a3c9cd537bd617c",
+    "url": "/crm-sys/css/chunk-vendors.e1364239.css"
+  },
+  {
+    "revision": "bd3a9d7af09b4fdfa8efd1693b20111e",
+    "url": "/crm-sys/img/My-IMG.bd3a9d7a.jpg"
+  },
+  {
+    "revision": "92036a60240e611cc64be9cee940465d",
+    "url": "/crm-sys/index.html"
+  },
+  {
+    "revision": "bc1bffd28446508e2c14",
+    "url": "/crm-sys/js/app.07d59715.js"
+  },
+  {
+    "revision": "47bf7a3c9cd537bd617c",
+    "url": "/crm-sys/js/chunk-vendors.c7b2102e.js"
+  },
+  {
+    "revision": "7e8516eb1be195337bc340af164091fc",
+    "url": "/crm-sys/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-sys/robots.txt"
+  }
+]);
